bbc95f223e1ae358833fbdd2e5f912dae463e64c
Tony Hyunghoon Sung <48133382+HS587885@users.noreply.github.com>
2025-05-17 17:23:08 +0900

    Title: 키를 기준으로 정렬, Time: 43ms, Memory: 16MB, Status: Passed - Codetree

---
+++ 250517/키를 기준으로 정렬/sort-by-height.py
@@ -0,0 +1,30 @@
+n = int(input())
+name = []
+height = []
+weight = []
+
+for _ in range(n):
+    n_i, h_i, w_i = input().split()
+    name.append(n_i)
+    height.append(int(h_i))
+    weight.append(int(w_i))
+
+# Please write your code here.
+class Person:
+    def __init__(self, name, height, weight):
+        self.name = name
+        self.height = height
+        self.weight = weight
+
+    def in_show(self):
+        print()
+
+person = []
+for i in range(len(name)):
+    p = Person(name[i], height[i], weight[i])
+    person.append(p)
+
+person.sort(key=lambda x: x.height) 
+
+for human in person: 
+    print(human.name, human.height, human.weight)
