98ca84e5f6a2dd588dac55c842e7eb6d4e4c1d19
Tony Hyunghoon Sung <48133382+HS587885@users.noreply.github.com>
2025-05-19 21:43:15 +0900

    Title: DateTime to DateTime, Time: 45ms, Memory: 16MB, Status: Passed - Codetree

--- 250519/DateTime to DateTime/datetime-to-datetime.py
+++ 250519/DateTime to DateTime/datetime-to-datetime.py
@@ -3,21 +3,11 @@ import sys
 a, b, c = map(int, sys.stdin.readline().split())
 
 # Please write your code here.
-day, hour, mins = 11,11,11
-elapsed_time = 0 
+start_minutes = (11 * 24 * 60) + (11 * 60) + 11
 
-while True:
-    if day == a and hour == b and mins == c:
-        break
-    elapsed_time += 1
-    mins += 1
+target_minutes = (a * 24 * 60) + (b * 60) + c
 
-    if mins == 60:
-        hour += 1
-        mins = 0
-    
-    if hour == 24:
-        day += 1
-        hour = 0
+elapsed_time = target_minutes - start_minutes
 
-print(elapsed_time)
+# 결과 출력 (음수면 -1 출력)
+print(elapsed_time if elapsed_time >= 0 else -1)
