2061345d645f01c6281034f8fbf0dd66ea241ab8
Tony Hyunghoon Sung <48133382+HS587885@users.noreply.github.com>
2025-05-19 21:38:16 +0900

    Title: DateTime to DateTime, Time: 1010ms, Memory: 16MB, Status: Time Limit Exceed - Codetree

---
+++ 250519/DateTime to DateTime/datetime-to-datetime.py
@@ -0,0 +1,21 @@
+a, b, c = map(int, input().split())
+
+# Please write your code here.
+day, hour, mins = 11,11,11
+elapsed_time = 0 
+
+while True:
+    if day == a and hour == b and mins == c:
+        break
+    elapsed_time += 1
+    mins += 1
+
+    if mins == 60:
+        hour += 1
+        mins = 0
+    
+    if hour == 24:
+        day += 1
+        hour = 0
+
+print(elapsed_time)
