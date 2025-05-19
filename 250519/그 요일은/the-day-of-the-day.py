bdfe2bdee94d5dcc675f9b3bb1c8ac94b519e8a0
Tony Hyunghoon Sung <48133382+HS587885@users.noreply.github.com>
2025-05-19 22:42:22 +0900

    Title: 그 요일은, Time: 58ms, Memory: 16MB, Status: Wrong Answer - Codetree

--- 250519/그 요일은/the-day-of-the-day.py
+++ 250519/그 요일은/the-day-of-the-day.py
@@ -1,21 +1,19 @@
-m1, d1, m2, d2 = map(int, input().split())
-A = input()
-
-# Please write your code here.
 import datetime
 
-target_day = A
+# 입력
+m1, d1, m2, d2 = map(int, input().split())
+A  = input().strip()
 
-# 시작일과 종료일
-start = datetime.date(2011, m1, d1)
-end = datetime.date(2011, m2, d2)
+# 시작일과 종료일 정의
+start = datetime.date(2024, m1, d1)
+end = datetime.date(2024, m2, d2)
 
-# 요일 카운터
+# 요일 카운트
 count = 0
 
-# 반복문으로 날짜 순회
+# 날짜 순회하면서 요일 비교
 while start <= end:
-    if start.strftime('%a') == target_day:
+    if start.strftime('%a') == A:
         count += 1
     start += datetime.timedelta(days=1)
 
