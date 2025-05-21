26ccd9f7502adbb9b57d9a9ac162427fe868ddb0
Tony Hyunghoon Sung <48133382+HS587885@users.noreply.github.com>
2025-05-21 22:31:51 +0900

    Title: 세 실수의 반올림, Time: 44ms, Memory: 16MB, Status: Passed - Codetree

---
+++ 250521/세 실수의 반올림/rounding-of-three-actual-numbers.py
@@ -0,0 +1,10 @@
+import sys
+
+a = float(sys.stdin.readline())
+b = float(sys.stdin.readline())
+c = float(sys.stdin.readline())
+
+print("%.3f" % round(a,3))
+print("%.3f" % round(b,3))
+print("%.3f" % round(c,3))
+
