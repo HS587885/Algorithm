e8ce367eb826cc1fab9f3f94efd058bc5ea65944
Tony Hyunghoon Sung <48133382+HS587885@users.noreply.github.com>
2025-05-25 20:59:55 +0900

    Title: 알파벳에 따른 평가, Time: 68ms, Memory: 18MB, Status: Passed - Codetree

---
+++ 250525/알파벳에 따른 평가/evaluation-by-alphabet.py
@@ -0,0 +1,14 @@
+a = input()
+
+if a is "S":
+    print("Superior")
+elif a is "A":
+   print("Excellent")
+elif a is "B":
+   print("Good")
+elif a is 'C':
+   print("Usually")
+elif a is 'D':
+   print("Effort")
+else:
+   print("Failure")
