b2855f71bf8a92871999b85a88d6bef30a319465
Tony Hyunghoon Sung <48133382+HS587885@users.noreply.github.com>
2025-05-29 17:24:15 +0900

    Title: 최댓값 구하기, Time: 44ms, Memory: 16MB, Status: Passed - Codetree

--- 250529/최댓값 구하기/maximum-value.py
+++ 250529/최댓값 구하기/maximum-value.py
@@ -1,8 +1,8 @@
 a,b,c = map(int, input().split())
 
-if a > b and a > c:
+if a >= b and a >= c:
     print(a)
-elif b > c and b > a:
+elif b >= a and b >= c: 
     print(b)
-elif c > a and c > b:
+else: 
     print(c)
