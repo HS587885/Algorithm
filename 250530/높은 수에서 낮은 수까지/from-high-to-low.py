10e39fd8fefa96a7f5898456b0b37b1acca882a1
Tony Hyunghoon Sung <48133382+HS587885@users.noreply.github.com>
2025-05-30 20:03:39 +0900

    Title: 높은 수에서 낮은 수까지, Time: 42ms, Memory: 16MB, Status: Passed - Codetree

--- 250530/높은 수에서 낮은 수까지/from-high-to-low.py
+++ 250530/높은 수에서 낮은 수까지/from-high-to-low.py
@@ -1,3 +1,8 @@
-a, b = map(int, input().split())
-answer = [i for i in range(b,a-1, -1)]
-print(*answer)
+a, b = map(int, input(). split())
+
+if a >= b:
+    for i in range (a, b - 1, -1):
+        print(i, end=' ')
+if a < b:
+    for i in range (b, a - 1, -1):
+        print(i, end=' ')
