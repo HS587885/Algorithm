902434cb5ab78d08d7275d3b3be24eb1db5ffcd0
Tony Hyunghoon Sung <48133382+HS587885@users.noreply.github.com>
2025-06-01 01:25:45 +0900

    Title: 벌금은 누구에게, Time: 72ms, Memory: 17MB, Status: Passed - Codetree

--- 250601/벌금은 누구에게/who-will-pay.py
+++ 250601/벌금은 누구에게/who-will-pay.py
@@ -5,12 +5,15 @@ student = [int(input()) for _ in range(M)]
 lst = {}
 
 s = deque(student)
+found = False  # <- 추가
+
 while s:
     std = s.popleft()
     lst[std] = lst.get(std, 0) + 1
     if lst[std] == K:
         print(std)
+        found = True
         break
 
-
-
+if not found:
+    print(-1)
