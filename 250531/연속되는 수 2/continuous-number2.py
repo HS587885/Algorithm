c24220648161f56172990a7170c90fb2021c4e1c
Tony Hyunghoon Sung <48133382+HS587885@users.noreply.github.com>
2025-05-31 19:31:28 +0900

    Title: 연속되는 수 2, Time: 63ms, Memory: 16MB, Status: Passed - Codetree

--- 250531/연속되는 수 2/continuous-number2.py
+++ 250531/연속되는 수 2/continuous-number2.py
@@ -1,10 +1,16 @@
 n = int(input())
 arr = [int(input()) for _ in range(n)]
 
-# Please write your code here.
-cnt = 0
-for i in range(n):
-    if i == 0 or arr[i] == arr[i - 1]:
-        cnt += 1
 
-print(cnt)
+
+max_count = 1
+current_count = 1
+
+for i in range(1, n):
+    if arr[i] == arr[i-1]:
+        current_count += 1
+        max_count = max(max_count, current_count)
+    else:
+        current_count = 1
+
+print(max_count)
