c3ead0943b7cf6bf7f685376f54379a679f7e09b
Tony Hyunghoon Sung <48133382+HS587885@users.noreply.github.com>
2025-06-04 01:15:26 +0900

    Title: 20대의 평균 나이, Time: 73ms, Memory: 18MB, Status: Runtime Error - Codetree

--- 250604/20대의 평균 나이/average-age-of-20.py
+++ 250604/20대의 평균 나이/average-age-of-20.py
@@ -1,12 +1,16 @@
 total = 0
 cnt = 0
+
 while True:
     n = int(input())
     if n > 29:
-        average = total / cnt
-        print(("%.2f" % round(average, 2)))
+        if cnt == 0:
+            print(0)
+        else:
+            average = total / cnt
+            print("%.2f" % round(average, 2))
         break
-
     else:
         total += n
         cnt += 1
+
