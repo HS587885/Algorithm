51301422d74daac4b297823398470e1fa9054eea
Tony Hyunghoon Sung <48133382+HS587885@users.noreply.github.com>
2025-06-04 01:08:58 +0900

    Title: 사각형의 넓이 계속 계산하기, Time: 41ms, Memory: 16MB, Status: Passed - Codetree

---
+++ 250604/사각형의 넓이 계속 계산하기/continue-calculating-width-of-the-rectangle.py
@@ -0,0 +1,9 @@
+while True:
+    w,h,c = map(str, input().split())
+    if c == "C":
+        print(int(w)* int(h))
+        break
+    else:
+        print(int(w) *int(h))
+
+    #print(int(w) * int(h))
