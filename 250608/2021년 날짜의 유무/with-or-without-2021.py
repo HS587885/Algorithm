8cf50e5ab0324bc105eca2a6c5dcc4c5f5f8983b
Tony Hyunghoon Sung <48133382+HS587885@users.noreply.github.com>
2025-06-08 00:10:13 +0900

    Title: 2021년 날짜의 유무, Time: 44ms, Memory: 16MB, Status: Passed - Codetree

--- 250608/2021년 날짜의 유무/with-or-without-2021.py
+++ 250608/2021년 날짜의 유무/with-or-without-2021.py
@@ -2,6 +2,8 @@ M, D = map(int, input().split())
 
 # Please write your code here.
 def f(m,d):
+    if m < 0 or m > 12:
+        return "No"
     days_in_month_2021 = [31, 28, 31, 30, 31, 30, 31, 31, 30, 31, 30, 31]
     return "Yes" if d <= days_in_month_2021[m-1] else "No"
 
