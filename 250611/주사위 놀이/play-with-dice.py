eec5a18bb786da0f7dc66c2844d4812b65363c52
Tony Hyunghoon Sung <48133382+HS587885@users.noreply.github.com>
2025-06-11 14:01:16 +0900

    Title: 주사위 놀이, Time: 45ms, Memory: 16MB, Status: Passed - Codetree

--- 250611/주사위 놀이/play-with-dice.py
+++ 250611/주사위 놀이/play-with-dice.py
@@ -1,5 +1,12 @@
 import sys
 
 lst = list(map(int, sys.stdin.readline().split()))
+# 배열에 주어진 수를 입력받아 저장
+count_arr = [0] * 7
+	
+for elem in lst:
+	count_arr[elem] += 1
+	
 for i in range(1, 7):
-    print(f'{i} - {lst.count(i)}')
+	print(f"{i} - {count_arr[i]}")
+
