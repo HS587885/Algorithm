e6615611abc5dcf246950f5a7d5248033b3e4fdd
Tony Hyunghoon Sung <48133382+HS587885@users.noreply.github.com>
2025-06-11 18:59:52 +0900

    Title: 나눗셈의 나머지, Time: 41ms, Memory: 16MB, Status: Passed - Codetree

--- 250611/나눗셈의 나머지/remainder-of-division.py
+++ 250611/나눗셈의 나머지/remainder-of-division.py
@@ -1,10 +1,14 @@
 a, b = map(int, input().split())
 
-remainders = []
-while a > 0:
-    remainder = a % b
-    remainders.append(remainder)
-    a = a // b
+# 나머지 등장 횟수를 저장할 리스트
+mod_list = [0] * b
 
-total = sum(remainders.count(i) ** 2 for i in set(remainders))
-print(total)
+while a > 1:
+    idx = a % b
+    mod_list[idx] += 1
+    a //= b
+
+# 등장 횟수의 제곱을 모두 더함
+ans = sum(x * x for x in mod_list)
+print(ans)
+
