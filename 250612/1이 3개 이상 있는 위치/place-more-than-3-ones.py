f4fe3bc308ed26373d613f9f1e4ca0f504de155f
Tony Hyunghoon Sung <48133382+HS587885@users.noreply.github.com>
2025-06-12 00:32:28 +0900

    Title: 1이 3개 이상 있는 위치, Time: 60ms, Memory: 17MB, Status: Wrong Answer - Codetree

--- 250612/1이 3개 이상 있는 위치/place-more-than-3-ones.py
+++ 250612/1이 3개 이상 있는 위치/place-more-than-3-ones.py
@@ -1,36 +1,21 @@
-from collections import deque
-
 n = int(input())
 grid = [list(map(int, input().split())) for _ in range(n)]
 
-# Please write your code here.
+dx = [0, -1, 1, 0, 0]
+dy = [0, 0, 0, -1, 1]
 
-dx = [-1,1,0,0]
-dy = [0,0,-1,1]
+result = 0
 
-n = len(grid)
-m = len(grid[0])
-
-x,y = 0,0
-def bfs(x,y):
-    q = deque([(x,y)])
-    cnt = 0
-    while q:
-        x,y = q.popleft()
-        for i in range(4):
-            nx = x + dx[i]
-            ny = y + dy[i]
-            if 0 <= nx < n and 0 <= ny < m and grid[nx][ny] == 1:
-                grid[nx][ny] = grid[x][y] + 1
-                q.append((nx,ny))
-                cnt += 1
-       
-    return cnt
-
-max_cnt = 0
 for i in range(n):
     for j in range(n):
-        if grid[i][j] == 1:
-            max_cnt = max(max_cnt, bfs(i, j))
+        cnt = 0
+        for d in range(5):
+            ni = i + dx[d]
+            nj = j + dy[d]
+            if 0 <= ni < n and 0 <= nj < n:
+                if grid[ni][nj] == 1:
+                    cnt += 1
+        if cnt >= 3:
+            result += 1
 
-print(max_cnt)
+print(result)
