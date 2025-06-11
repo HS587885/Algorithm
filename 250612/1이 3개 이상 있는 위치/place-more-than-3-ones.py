a654107167677078a9bc4fff0988d2423ea4a777
Tony Hyunghoon Sung <48133382+HS587885@users.noreply.github.com>
2025-06-12 00:34:20 +0900

    Title: 1이 3개 이상 있는 위치, Time: 60ms, Memory: 17MB, Status: Passed - Codetree

--- 250612/1이 3개 이상 있는 위치/place-more-than-3-ones.py
+++ 250612/1이 3개 이상 있는 위치/place-more-than-3-ones.py
@@ -1,21 +1,22 @@
 n = int(input())
 grid = [list(map(int, input().split())) for _ in range(n)]
 
-dx = [0, -1, 1, 0, 0]
-dy = [0, 0, 0, -1, 1]
+# 상하좌우
+dx = [-1, 1, 0, 0]
+dy = [0, 0, -1, 1]
 
 result = 0
 
 for i in range(n):
     for j in range(n):
-        cnt = 0
-        for d in range(5):
+        count = 0
+        for d in range(4):  # 자기 자신 제외하고 4방향만 탐색
             ni = i + dx[d]
             nj = j + dy[d]
             if 0 <= ni < n and 0 <= nj < n:
                 if grid[ni][nj] == 1:
-                    cnt += 1
-        if cnt >= 3:
+                    count += 1
+        if count >= 3:
             result += 1
 
 print(result)
