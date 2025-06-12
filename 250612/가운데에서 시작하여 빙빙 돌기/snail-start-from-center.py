3d42370b07ffd94cae2738babf09dcf27e37be8d
Tony Hyunghoon Sung <48133382+HS587885@users.noreply.github.com>
2025-06-12 23:57:49 +0900

    Title: 가운데에서 시작하여 빙빙 돌기, Time: 69ms, Memory: 18MB, Status: Passed - Codetree

--- 250612/가운데에서 시작하여 빙빙 돌기/snail-start-from-center.py
+++ 250612/가운데에서 시작하여 빙빙 돌기/snail-start-from-center.py
@@ -1,34 +1,38 @@
+import sys
+input = sys.stdin.readline
+
 n = int(input())
 grid = [[0] * n for _ in range(n)]
 
-# 방향: 오른쪽, 위, 왼쪽, 아래 (시계 반대)
+# Please write your code here.
+
+# 오른쪽, 위, 왼쪽, 아래
 dx = [0, -1, 0, 1]
 dy = [1, 0, -1, 0]
 
-x = y = n // 2
+x = n // 2
+y = n // 2
+
 grid[x][y] = 1
-
-num = 2
-step = 1
 dir_num = 0
+cnt = 1
+cur = 0
 
-while num <= n * n:
-    for _ in range(2):  # 같은 step 수로 두 번 방향 전환
-        for _ in range(step):
-            x += dx[dir_num]
-            y += dy[dir_num]
-            if 0 <= x < n and 0 <= y < n:
-                grid[x][y] = num
-                num += 1
-                if num > n * n:
-                    break
+for i in range(1,n * n):
+    nx, ny = x + dx[dir_num], y + dy[dir_num]
+    if not (0 <= nx < n and 0 <= ny < n) or cur >= cnt or grid[nx][ny] != 0:
         dir_num = (dir_num + 1) % 4
-        if num > n * n:
-            break
-    step += 1
+        if dir_num == 0 or dir_num == 2:
+            cnt += 1
+        cur = 0
 
-
+    
+    x = x + dx[dir_num]
+    y = y + dy[dir_num]
+    grid[x][y] = i + 1
+    cur += 1
+    
 for i in range(n):
     for j in range(n):
-        print(grid[i][j], end=" ")
+        print(grid[i][j], end = " ")
     print()
