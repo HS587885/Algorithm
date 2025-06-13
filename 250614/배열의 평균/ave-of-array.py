8ca8d0ee171d63d32eeb84e5ac39a5bb7a33e055
Tony Hyunghoon Sung <48133382+HS587885@users.noreply.github.com>
2025-06-14 00:30:41 +0900

    Title: 배열의 평균, Time: 43ms, Memory: 16MB, Status: Passed - Codetree

--- 250614/배열의 평균/ave-of-array.py
+++ 250614/배열의 평균/ave-of-array.py
@@ -6,7 +6,7 @@ arr = [
 # 가로 평균
 for i in range(2):
     row_avg = sum(arr[i]) / len(arr[i])
-    print(row_avg, end = ' ')
+    print(("%.1f" %row_avg), end = ' ')
 print()
 # 세로 평균  
 for j in range(len(arr[0])):
@@ -14,7 +14,7 @@ for j in range(len(arr[0])):
     for i in range(2):
         col_sum += arr[i][j]
     col_avg = col_sum / 2
-    print(col_avg, end = ' ')
+    print(("%.1f" % col_avg), end = ' ')
 print()
 # 전체 평균
 total = 0
@@ -24,5 +24,5 @@ for i in range(2):
         total += arr[i][j]
         count += 1
 total_avg = total / count
-print(total_avg, end = ' ')
+print(("%.1f" % total_avg), end = ' ')
 
