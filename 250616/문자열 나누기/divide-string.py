f8f67fd2fbdfb0a2753771b1b6101421a3d0d12c
Tony Hyunghoon Sung <48133382+HS587885@users.noreply.github.com>
2025-06-16 23:52:14 +0900

    Title: 문자열 나누기, Time: 44ms, Memory: 16MB, Status: Wrong Answer - Codetree

--- 250616/문자열 나누기/divide-string.py
+++ 250616/문자열 나누기/divide-string.py
@@ -5,7 +5,6 @@ N = int(input())
 word = input().strip().replace(" ", "")
 for i in range(5, len(word), 5):
     print(word[i-5:i])
-print(word[-1])
 
-
-
+if len(word) % 5 != 0:
+    print(word[len(word) - len(word) % 5:])
