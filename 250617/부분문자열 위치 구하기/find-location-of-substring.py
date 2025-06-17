5bcf4a75a4f34319d7cd4772ba0c907ba4d4d817
Tony Hyunghoon Sung <48133382+HS587885@users.noreply.github.com>
2025-06-17 22:55:16 +0900

    Title: 부분문자열 위치 구하기, Time: 43ms, Memory: 16MB, Status: Wrong Answer - Codetree

--- 250617/부분문자열 위치 구하기/find-location-of-substring.py
+++ 250617/부분문자열 위치 구하기/find-location-of-substring.py
@@ -2,6 +2,14 @@ input_str = input()
 target_str = input()
 
 # Please write your code here.
-result = input_str.replace(target_str, '#')
-answer = result.count('#')
-print(answer if answer != 0 else -1)
+s = input_str
+length = len(s)
+cnt = 0
+
+t1, t2 = target_str[0], target_str[1]
+for i in range(length - 1):
+    if s[i] == t1 and s[i + 1] == t2:
+        cnt += 1
+
+
+print(cnt if cnt > 0 else -1)        
